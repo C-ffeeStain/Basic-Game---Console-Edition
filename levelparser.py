c7316862bee6c956ac19817e14a5e4b54aeb3149
C-ffeeStain <deaconavery2006@gmail.com>
2021-02-18 18:17:42 -0600

    made levelparser paths crossplatform

--- levelparser.py
+++ levelparser.py
@@ -17,7 +17,7 @@ class Level:
         "x": 4,
         "y": 2,
         "prev_x": 4,
-        "prev_y": 2
+        "prev_y": 3
     }
     score_appear_interval = 3
     unlock_exit_score = score_appear_interval * 3
@@ -25,7 +25,7 @@ class Level:
         self.name = name
 
     def set_vars(self):
-        with open("levels\\" + self.name + ".json") as raw:
+        with open("levels/" + self.name + ".json") as raw:
             level_json = json.loads(raw.read())
         self.score_appear_interval = level_json["score_appear_interval"]
         self.unlock_exit_score = level_json["unlock_exit_score"]
@@ -36,7 +36,7 @@ class Level:
         
     def parse(self):
         self.area = []
-        with open("levels\\" +self.name + ".lvl", "r") as lvl:
+        with open("levels/" +self.name + ".lvl", "r") as lvl:
             self.area = [split(line) for line in lvl]
             for line in self.area:
                 for tile in line:
